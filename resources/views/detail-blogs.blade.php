8b5f576ac4ff48d1f2373c13293679b2336e4cee
mrziddux <ziyadatulkhair.id@gmail.com>
2021-09-24 10:30:29 +0700

    Membuat perubahan pada login, detail blog, dan memperbaiki navbar dan detail

--- resources/views/detail-blogs.blade.php
+++ resources/views/detail-blogs.blade.php
@@ -1,8 +1,8 @@
 <x-applayout title="Detail Blogs">
 	<div class="container-fluid py-5">
 		<div class="container">
-			<div class="row">
-				<div class="col-lg-8">
+			<div class="row g-4">
+				<div class="col-lg-8 ww-break-word">
 					<img src="/images/uploads/articles/{{ $data->thumbnail }}" alt="Blog Image" class="img-full2 d-block mb-4"	
 					>	
 					<h2 class="t-bold t-color-primary">{{ $data->title }}</h2>
@@ -20,26 +20,6 @@
 							<a href="/blogs/tag/{{ $tags[$i] }}" class="btn btn-sm border-color-primary t-color-primary hover-bg-primary hover-text-white transition">{{ $tags[$i] }}</a>
 						@endfor
 					</div>
-
-					<h2 class="t-bold t-color-primary mt-5 mb-5">Komentar</h2>
-					<form action="" method="">
-						<div class="form-group mb-5">
-							<input type="text" class="form-control form-custom" name="nama" id="nama" required>
-							<label for="nama" class="form-label label-custom">Nama</label>
-						</div>
-						<div class="form-group mb-5">
-							<input type="email" class="form-control form-custom" name="email" id="email" required>
-							<label for="email" class="form-label label-custom">Email</label>
-						</div>
-						<div class="form-group mb-5">
-							<textarea type="text" class="form-control textarea-custom" name="nama" id="nama" rows="4" required></textarea>
-							<label for="nama" class="form-label label-textarea">Komentar</label>
-						</div>
-						<div class="d-flex justify-content-between">
-							<button type="button" class="btn bg-color-primary text-white hover-btn-color-primary transition">Kirim Komentar</button>
-							<button type="reset" class="btn btn-sm border-color-secondary hover-bg-secondary hover-text-white t-color-secondary transition">Reset Komentar</button>
-						</div>
-					</form>
 				</div>
 				<div class="col-lg-4">
 					<h4 class="t-bold t-color-primary mb-3">Search</h4>
@@ -72,6 +52,25 @@
 					@endforeach
 					
 				</div>
+				<h2 class="t-bold t-color-primary mt-5 mb-5">Komentar</h2>
+				<form action="" method="">
+					<div class="form-group mb-5">
+						<input type="text" class="form-control form-custom" name="nama" id="nama" required>
+						<label for="nama" class="form-label label-custom">Nama</label>
+					</div>
+					<div class="form-group mb-5">
+						<input type="email" class="form-control form-custom" name="email" id="email" required>
+						<label for="email" class="form-label label-custom">Email</label>
+					</div>
+					<div class="form-group mb-5">
+						<textarea type="text" class="form-control textarea-custom" name="nama" id="nama" rows="4" required></textarea>
+						<label for="nama" class="form-label label-textarea">Komentar</label>
+					</div>
+					<div class="d-flex justify-content-between">
+						<button type="button" class="btn bg-color-primary text-white hover-btn-color-primary transition">Kirim Komentar</button>
+						<button type="reset" class="btn btn-sm border-color-secondary hover-bg-secondary hover-text-white t-color-secondary transition">Reset Komentar</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="/">
       <img src="/images/himed_logo.png" alt="himed logo" height="24">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
